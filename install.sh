68f1375008fb0f077cdb549b462ba9314fdf61a2
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-04-03 11:58:16 -0600

    instalación de typescript, ejs, nodemon, jison, express

---
+++ install.sh
@@ -0,0 +1,10 @@
+clear
+npm init --yes
+npm install express
+npm install @types/express
+npm install ejs
+npm install -g ts-node
+npm install -g typescript
+npm install @types/node
+npm install -g nodemon
+npm install -g jison
