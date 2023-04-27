546c22ecfc6b6ec37ae1302130ffba9298fea7a0
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-04-26 23:18:41 -0600

    Reporte AST

--- FrontEnd/js/functions.js
+++ FrontEnd/js/functions.js
@@ -1,16 +1,30 @@
 function analyze() {
-    let code1 = editor.getValue()
-    code1 = code1.replace(/\\/g,'\\\\')
-    code1 = code1.replace(/\t/g,'    ')
-    code1 = code1.replace(/\r?\n|\r/g,'\\n')
-    code1 = code1.replace(/"/g,'\\"')
     fetch(`${path}/interpreter/parser`,{
         method: 'POST',
         headers,
-        body: `{"code":"${code1}"}`
+        body: `{"code":"${getCode()}"}`
     })
     .then(response => response.json())
     .then(response => {
         out.setOption('value',response.console.join('\n'))
     })
+}
+function graphAST() {
+    fetch(`${path}/interpreter/parserAST`,{
+        method: 'POST',
+        headers,
+        body: `{"code":"${getCode()}"}`
+    })
+    .then(response => response.json())
+    .then(response => {
+        d3.select('#graphAST').graphviz().scale(0.8).height(document.getElementById('graphAST').clientHeight).width(890*1.9).renderDot(response.console)
+    })
+}
+function getCode() {
+    let code = editor.getValue()
+    code = code.replace(/\\/g,'\\\\')
+    code = code.replace(/\t/g,'    ')
+    code = code.replace(/\r?\n|\r/g,'\\n')
+    code = code.replace(/"/g,'\\"')
+    return code
 }
